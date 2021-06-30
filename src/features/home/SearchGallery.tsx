01b645c5bf086ce6a1a821635a663cafc78d8e6d
Dandy Ling <dandyling@gmail.com>
2021-06-30 11:44:03 +0800

    feat: Add the tags

--- src/features/home/SearchGallery.tsx
+++ src/features/home/SearchGallery.tsx
@@ -1,5 +1,7 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Tag, TagLabel } from "@chakra-ui/react";
 import React, { ChangeEvent, useState } from "react";
+import { useEffect } from "react";
+import { Basic } from "unsplash-js/dist/methods/photos/types";
 import { Gallery } from "../../components/Gallery";
 import { GalleryPager } from "./GalleryPager";
 import { useCurrentPageNumber } from "./useCurrentPageNumber";
@@ -9,10 +11,23 @@ interface SearchGalleryProps {
   query: string;
 }
 
+interface BasicTag {
+  type: string;
+  title: string;
+}
+interface SearchBasic extends Basic {
+  tags?: BasicTag[];
+}
+
 export const SearchGallery = (props: SearchGalleryProps) => {
   const { query } = props;
   const [pageSize, setPageSize] = useState(DEFAULT_PAGE_SIZE);
+  const [filter, setFilter] = useState("");
   const currentPageNumber = useCurrentPageNumber();
+
+  useEffect(() => {
+    setFilter("");
+  }, [query, currentPageNumber]);
 
   const handlePageSizeChange = (e: ChangeEvent<HTMLSelectElement>) => {
     setPageSize(Number(e.currentTarget.value));
@@ -31,8 +46,22 @@ export const SearchGallery = (props: SearchGalleryProps) => {
     return null;
   }
 
+  const tags = getTags(data.response.results);
+  const sortedTags = tags.sort((a, b) => (a > b ? 1 : -1));
+
+  const photos = getFilteredPhotos(data.response.results, filter);
+
   return (
     <Flex direction="column" maxWidth="100vw">
+      <Flex px="2" overflowX="scroll">
+        {sortedTags.map((tag) => {
+          return (
+            <Tag whiteSpace="nowrap" minWidth="auto" size="md" mx="1">
+              <TagLabel onClick={() => setFilter(tag)}>{tag}</TagLabel>
+            </Tag>
+          );
+        })}
+      </Flex>
       <GalleryPager
         currentPageNumber={currentPageNumber ?? 1}
         pageSize={pageSize}
@@ -41,7 +70,28 @@ export const SearchGallery = (props: SearchGalleryProps) => {
         paddingBottom="1.5"
         paddingX="6"
       />
-      <Gallery photos={data.response.results} />
+      <Gallery photos={photos} />
     </Flex>
   );
+};
+
+const getTags = (results: Basic[]): string[] => {
+  const tagsHash: Record<string, number> = {};
+  results.forEach((result: SearchBasic) => {
+    result.tags?.forEach((tag) => {
+      tagsHash[tag.title] = 1;
+    });
+  });
+  const tags = Object.keys(tagsHash);
+
+  return tags;
+};
+
+const getFilteredPhotos = (results: Basic[], filter: string) => {
+  return filter === ""
+    ? results
+    : results.filter((result: SearchBasic) => {
+        const tagTitles = result.tags?.map((t) => t.title);
+        return tagTitles?.includes(filter);
+      });
 };
